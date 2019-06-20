80881bc3f1d8d1f87bfa73f485abdb10aeff9d7e
Edreyhernandez <35716388+Edreyhernandez@users.noreply.github.com>
2019-06-20 15:21:12 -0500

    Add files via upload

---
+++ src/components/About.vue
@@ -0,0 +1,34 @@
+<template>
+    <section class="about">
+        <div class="container">
+            <div class="about__content">
+                <div class="about__description">
+                    <h2>    
+                        ¿Por qué en Quintana Roo?
+                    </h2>
+                    <p>
+                        Poseemos un enorme potencial para el desarrollo tecnológico y estamos seguros de que fomentando el uso de tecnología podemos generar un ecosistema que generé emprendimiento e innovación para mejorar nuestro entorno social y económico en ámbito tecnológico.
+                    </p>
+                    <br>
+                    <a href="#" class="button--primary">More info</a>
+                </div>
+                <div class="about__description">
+                    <img src="../assets/imagenes/about.jpg" alt="">
+                </div>
+            </div>
+        </div>
+        <svg class="bg-white" width="100%" height="100%" version="1.1" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="none">
+          <path d="M0,0 C6.83050094,50 15.1638343,75 25,75 C41.4957514,75 62.4956597,0 81.2456597,0 C93.7456597,0 99.9971065,0 100,0 L100,100 L0,100" fill="#ffffff"></path>
+        </svg>
+    </section>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="banner">
+        <img src="../assets/imagenes/banner.jpg" alt="">
+            <!--https://trello-attachments.s3.amazonaws.com/5ce973774eb4d74e7b37820b/5ce976218a36877f7e27f5c4/b10f546c2263ad346de98c416205c65d/IMG_20190425_225244.jpeg-->
+        <div class="banner__content">
+            <div class="container">
+                <h2 class="banner__title">
+                    Sloth Day 2019
+                </h2>
+                <p class="banner__subtitle">
+                    Es un evento que busca reunir a desarrolladores, diseñadores, amantes del marketing y tecnologías para hacer networking e intercambio de ideas a través de conferencias, charlas y talleres.
+                </p>
+            </div>
+        </div>
+        <svg class="path-banner" width="100%" height="100%" version="1.1" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="none">
+          <path d="M0,0 C6.83050094,50 15.1638343,75 25,75 C41.4957514,75 62.4956597,0 81.2456597,0 C93.7456597,0 99.9971065,0 100,0 L100,100 L0,100" fill="#ffffff"></path>
+        </svg>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'Banner'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
